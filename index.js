77a744f3594dc5363e9ad8b8936567647ceeabae
Max <you@example.com>
2023-04-01 16:48:02 +0600

    basic shit, my logic. bullshit logic

---
+++ index.js
@@ -0,0 +1,28 @@
+function generatePass() {
+  pass_view = document.getElementById("gen_pass");
+  pass_view.innerHTML = generator();
+}
+
+function generator() {
+  //gonna do this for 12 digit password
+  // A-Z = 65-90
+  // a-z = 97-122
+  // 0-9 = 48-57
+  const options = ["A-Z", "a-z", "0-9"];
+  const limits = ["65-90", "97-122", "48-57"];
+  let generatedString = "";
+  for (let index = 0; index <= 12; index++) {
+    let randomLimit = limits[Math.floor(Math.random() * options.length)];
+    let max_min = randomLimit.split("-");
+    let generatedNumber = getRandomArbitrary(
+      Number(max_min[0]),
+      Number(max_min[1])
+    );
+    generatedString = generatedString + String.fromCharCode(generatedNumber);
+  }
+  return generatedString;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
